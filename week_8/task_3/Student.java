45d5dee4ac6022b8f92c8fb585a99f235511a812
Freddiiy <frederikgaller@live.dk>
2021-02-25 12:52:29 +0100

    asd

---
+++ week_8/task_3/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int age;
+    boolean isFemale;
+    int datamatikerTeam;
+
+    public Student(String tmpName, int tmpAge, boolean tmpIsFemale, int tmpDatamatikerTeam) {
+        this.name = tmpName;
+        this.age = tmpAge;
+        this.isFemale = tmpIsFemale;
+        this.datamatikerTeam = tmpDatamatikerTeam;
+    }
+}
